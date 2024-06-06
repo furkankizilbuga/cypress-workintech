5c34d22f5301dce861cc32db942b03ee2f3ea2d6
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-06-06 17:17:42 +0300

    login and success page

---
+++ furkan-cypress/src/components/Login.jsx
@@ -0,0 +1,150 @@
+import { useState, useEffect } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+    terms: false,
+  });
+
+  const history = useHistory();
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const emailListener = (event) => {
+    setErrors({
+      ...errors,
+      [event.target.name]: !validateEmail(event.target.value),
+    });
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const passwordListener = (event) => {
+    setErrors({
+      ...errors,
+      [event.target.name]: !(event.target.value.trim().length > 4),
+    });
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const checkBoxListener = (event) => {
+    setErrors({
+      ...errors,
+      [event.target.name]: !event.target.checked,
+    });
+    setForm({ ...form, [event.target.name]: event.target.checked });
+  };
+
+  useEffect(() => {
+    if (
+      validateEmail(form.email) &&
+      form.password.trim().length > 4 &&
+      form.terms
+    ) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [form]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!isValid) {
+      return;
+    }
+
+    axios
+      .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+      .then((res) => {
+        const user = res.data.find(
+          (item) => item.password == form.password && item.email == form.email
+        );
+        if (user) {
+          setForm(initialForm);
+          history.push('/success');
+        } 
+      });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={emailListener}
+          value={form.email}
+          invalid={errors.email}
+        />
+        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={passwordListener}
+          value={form.password}
+          invalid={errors.password}
+        />
+        {errors.password && (
+          <FormFeedback>{errorMessages.password}</FormFeedback>
+        )}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          onChange={checkBoxListener}
+          invalid={errors.terms}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button color="primary" disabled={!isValid}>
+          Sign In
+        </Button>
+      </FormGroup>
+    </Form>
+  );
+}
---
+++ furkan-cypress/src/components/Success.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Success() {
+
+
+    return(
+        <>
+            <h2>Giriş Başarılı!</h2>
+        </>
+    )
+}
